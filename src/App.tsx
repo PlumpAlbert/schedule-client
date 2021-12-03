34460dcc11eb79fbbb8e0c70e24563ac8136bcc6
plump_albert <plumpalbert@gmail.com>
2021-12-03 12:42:41 +0300

    Displaying search icon in page's header

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
     return (
         <div className={appClassName}>
             <PageHeader
+                menuIsShown={showMenu}
                 onMenuClick={menuButtonClicked}
                 onTodayClick={todayButtonClicked}
             />
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,36 +1,97 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router";
 import MenuIcon from "@mui/icons-material/Menu";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowRightIcon from "@mui/icons-material/ArrowForward";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
 import "../styles/PageHeader.scss";
 
 interface IProps {
     onMenuClick: React.MouseEventHandler;
     onTodayClick: React.MouseEventHandler;
+    menuIsShown: boolean;
 }
 
-function PageHeader(props: IProps) {
-    const onMenuClick = useCallback(
-        e => {
-            props.onMenuClick(e);
-        },
-        [props.onMenuClick]
-    );
-    const onTodayClick = useCallback(
-        e => {
-            props.onTodayClick(e);
-        },
-        [props.onTodayClick]
-    );
+function PageHeader({ onMenuClick, onTodayClick, menuIsShown }: IProps) {
+    const [showSearchInput, setShowSearchInput] = useState(false);
+    const location = useLocation();
+    const searchInputRef = useRef<HTMLInputElement>(null);
+
+    const handleMenuClick = useCallback(e => onMenuClick(e), [onMenuClick]);
+    const handleTodayClick = useCallback(e => onTodayClick(e), [onTodayClick]);
+    const handleSearchIconClick = useCallback(() => {
+        if (searchInputRef.current) {
+            searchInputRef.current.focus();
+        }
+        setShowSearchInput(true);
+    }, [setShowSearchInput]);
+    const handleEnterClick = useCallback(() => {
+        alert("Should implement this feature");
+    }, []);
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case "/":
+                setShowSearchInput(true);
+                break;
+            default:
+                break;
+        }
+    }, [location.pathname]);
+
     return (
         <div className="page-header">
             <MenuIcon
-                onClick={onMenuClick}
+                onClick={handleMenuClick}
                 classes={{ root: "page-header__menu-icon" }}
             />
-            <CalendarTodayIcon
-                classes={{ root: "page-header__calendar-icon" }}
-                onClick={onTodayClick}
-            />
+            {showSearchInput || menuIsShown ? (
+                <TextField
+                    className="page-header__search-input-wrapper"
+                    variant="standard"
+                    onBlur={e => {
+                        if (!e.target.value) {
+                            console.log("setShowSearchInput - false");
+                            setShowSearchInput(false);
+                        }
+                    }}
+                    placeholder="Введите название группы"
+                    InputProps={{
+                        inputRef: searchInputRef,
+                        classes: {
+                            root: `page-header__search-input${
+                                showSearchInput
+                                    ? " page-header__search-input--display"
+                                    : ""
+                            }`
+                        },
+                        startAdornment: (
+                            <InputAdornment
+                                position="start"
+                                onClick={handleSearchIconClick}
+                            >
+                                <SearchIcon className="page-header__search-input-icon" />
+                            </InputAdornment>
+                        ),
+                        endAdornment: (
+                            <InputAdornment
+                                className="page-header__search-input-enter"
+                                position="end"
+                                onClick={handleEnterClick}
+                            >
+                                <ArrowRightIcon />
+                            </InputAdornment>
+                        )
+                    }}
+                />
+            ) : (
+                <CalendarTodayIcon
+                    classes={{ root: "page-header__calendar-icon" }}
+                    onClick={handleTodayClick}
+                />
+            )}
         </div>
     );
 }
