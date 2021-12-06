ab6ddc25360026e244e418526daa8813d765b1ce
plump_albert <plumpalbert@gmail.com>
2021-12-06 23:11:06 +0300

    Implemented method for user authentication

--- src/API.ts
+++ src/API.ts
@@ -18,7 +18,7 @@ interface IResponse<T = any> {
     message?: string;
 }
 
-export default class ScheduleAPI {
+export default abstract class ScheduleAPI {
     private static HOST: string = "https://www.plumpalbert.xyz/api";
     /**
      * Method for fetching group's schedule
@@ -36,5 +36,28 @@ export default class ScheduleAPI {
             ...s,
             time: new Date(`1999-01-13T${s.time}`)
         }));
+    };
+
+	/**
+	 * Method to authenticate user's
+	 * @param login User's login
+	 * @param password User's password
+	 * @returns {boolean} True on success, false otherwise
+	 */
+    static authenticate = async (login: string, password: string) => {
+        const jsonText = await fetch(`${ScheduleAPI.HOST}/auth`, {
+            method: "POST",
+            cache: "no-cache",
+            // credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                login,
+                password
+            })
+        });
+		const result = await jsonText.json();
+		return !result.error;
     };
 }
