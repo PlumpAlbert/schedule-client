721b9cade6c39389ba5dc98c8935f6a6a63fb520
plump_albert <plumpalbert@gmail.com>
2021-12-08 20:49:10 +0300

    Showing user data in menu when user is logged in

--- src/components/MenuSlider.tsx
+++ src/components/MenuSlider.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import LogoutIcon from "@mui/icons-material/Logout";
 import EditIcon from "@mui/icons-material/Edit";
@@ -6,18 +6,25 @@ import PageFooter from "./PageFooter";
 import LandingPage from "../pages/LandingPage";
 
 import "../styles/MenuSlider.scss";
+import {IUser, UserType} from "../types";
 
 interface IProps {
 	showMenu: boolean;
 }
 
-function MenuSlider() {
-	const [userName, setUserName] = useState(
-		window.localStorage.getItem("username")
+function UserDataView() {
+	const [user, setUser] = useState<IUser>(
+		JSON.parse(sessionStorage.getItem("user") || "")
 	);
-	const [userGroup, setUserGroup] = useState(
-		window.localStorage.getItem("group")
-	);
+	const groupName = useMemo(() => {
+		const words = user.group.specialty.split(" ");
+		const year = user.group.year.toString();
+		return (
+			words.reduce((result, word) => result + word[0].toUpperCase(), "") +
+			"-" +
+			year.slice(-2)
+		);
+	}, [user.group.specialty, user.group.year]);
 
 	return (
 		<>
@@ -29,20 +36,20 @@ function MenuSlider() {
 						alt="user-avatar"
 						className="user-pic"
 					/>
-					<h2 className="user-name">
-						{userName ? userName : "No name"}
-					</h2>
-					<p className="user-group">{userGroup || "Unknown group"}</p>
+					<h2 className="user-name">{user.name}</h2>
+					<p className="user-group">{groupName}</p>
 				</div>
 			</div>
 			<div className="menu-settings">
 				<h3 className="menu-settings__header">Настройки</h3>
-				<p className="menu-settings__option">
-					<EditIcon classes={{root: "option__icon"}} />
-					<span className="option__text">
-						Редактировать расписание
-					</span>
-				</p>
+				{user.type === UserType.ADMIN && (
+					<p className="menu-settings__option">
+						<EditIcon classes={{root: "option__icon"}} />
+						<span className="option__text">
+							Редактировать расписание
+						</span>
+					</p>
+				)}
 				<p className="menu-settings__option">
 					<ManageAccountsIcon classes={{root: "option__icon"}} />
 					<span className="option__text">Изменить группу</span>
@@ -57,10 +64,10 @@ function MenuSlider() {
 }
 
 export default ({showMenu}: IProps) => {
-	const userInfo = window.localStorage.getItem("user");
+	const userInfo = sessionStorage.getItem("user");
 	return (
 		<div className={`app-menu${showMenu ? " active" : ""}`}>
-			{userInfo ? <MenuSlider /> : <LandingPage />}
+			{userInfo ? <UserDataView /> : <LandingPage />}
 			<PageFooter />
 		</div>
 	);
