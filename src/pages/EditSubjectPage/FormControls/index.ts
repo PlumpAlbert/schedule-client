0bba5ab9fed126b91b97e4b71f4a2af8202b412e
plump_albert <plumpalbert@gmail.com>
2021-12-23 13:29:58 +0300

    Removed unneeded `id` property

--- src/pages/EditSubjectPage/FormControls/index.ts
+++ src/pages/EditSubjectPage/FormControls/index.ts
@@ -4,14 +4,12 @@ import PropTypes from "prop-types";
 
 export function propTypes<T>(validator: T) {
 	return {
-		id: PropTypes.number.isRequired,
 		dispatch: PropTypes.func.isRequired,
 		value: validator
 	};
 }
 
 export interface IProps<T> {
-	id: number;
 	dispatch: React.Dispatch<AnyAction>;
 	value: T;
 }
